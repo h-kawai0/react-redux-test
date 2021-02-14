6312d367b5ec80b9c249e0ffe3d5a652cb62ef6c
h.kawai <hironori86@gmail.com>
2021-02-14 20:10:28 +0900

    fix typo for combineReducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux;'
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 
@@ -10,7 +10,7 @@ import reducer from './reducers';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
